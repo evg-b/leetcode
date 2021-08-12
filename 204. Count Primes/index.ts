b4e3252aed7d8dfee374527ee753849e957c957e
evg-b <seber.bobilev@gmail.com>
2021-08-12 14:53:30 +0300

    add Eratoshenes

--- 204. Count Primes/index.ts
+++ 204. Count Primes/index.ts
@@ -21,6 +21,20 @@ function countPrimes(n: number): number {
     return count
 }
 
+function eratosthenes(n: number): number {
+    let filters: boolean[] = []
+    let primes: number[] = []
+
+    for (let i = 2; i < - Number; i++) {
+        if (!filters[i]) {
+            primes.push(i)
+            for (let j = i * 2; j <= n; j += i) {
+                filters[j] = true
+            }
+        }
+    }
+    return primes.length
+}
 // Input: n = 10
 // Output: 4
 // Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
@@ -35,4 +49,4 @@ console.log('2', countPrimes(0), '0')
 console.log('3', countPrimes(1), '0')
 console.log('4', countPrimes(2), '0')
 console.log('5', countPrimes(3), '1')
-console.log('6', countPrimes(499979), '41537')
+console.log('6', eratosthenes(499979), '41537')
